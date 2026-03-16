d84c7bc9139f7e15d84bbf3195e6f855ac9c7f9b
Kingston Tsai <65468855+Prestopy@users.noreply.github.com>
2026-03-16 09:06:22 +0800

    Some code?

--- src/main/java/frc/robot/Target.java
+++ src/main/java/frc/robot/Target.java
@@ -1,6 +1,12 @@
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.Radians;
+
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Distance;
 
 public class Target {
     private Translation3d target = null;
@@ -12,7 +18,10 @@ public class Target {
     }
 
     public void moveToPosition() {
+        Distance dist = Meters.of(Math.sqrt(target.getX()*target.getX() + target.getY()*target.getY()));
 
+        Angle baseRotation = Radians.of(Math.atan2(target.getY(), target.getX()));
+        Translation2d slicedTarget = new Translation2d(dist.in(Meters), target.getY()); // ignoring base rotation
     }
 
     public Translation3d getPosition() {
